412f91ff2e57aaa38ae6ba3f21e90d980afd9e57
tran thanh tu <tttuab@gmail.com>
2022-12-01 00:25:04 +0700

    update

--- src/screens/ListOrderScreen/index.js
+++ src/screens/ListOrderScreen/index.js
@@ -127,61 +127,10 @@ import {
       color: Colors.DEFAULT_BLACK,
       marginLeft: 10,
     },
-    amountContainer: {
-      marginHorizontal: Display.setWidth(4),
-      paddingVertical: 20,
-      borderBottomWidth: 0.5,
-    },
-    amountSubContainer: {
-      flexDirection: "row",
-      alignItems: "center",
-      justifyContent: "space-between",
-      marginVertical: 3,
-    },
-    amountLabelText: {
-      fontSize: 15,
-      fontWeight: "600",
-      lineHeight: 15 * 1.4,
-      color: Colors.DEFAULT_GREEN,
-    },
-    amountText: {
-      fontSize: 15,
-      fontWeight: "600",
-      lineHeight: 15 * 1.4,
-      color: Colors.DEFAULT_BLACK,
-    },
-    totalContainer: {
-      marginHorizontal: Display.setWidth(4),
-      paddingVertical: 15,
-      borderBottomWidth: 0.5,
-      flexDirection: "row",
-      justifyContent: "space-between",
-    },
-    totalText: {
-      fontSize: 20,
-      fontWeight: "600",
-      lineHeight: 20 * 1.4,
-      color: Colors.DEFAULT_BLACK,
-    },
-    checkoutButton: {
-      flexDirection: "row",
-      width: Display.setWidth(80),
-      backgroundColor: Colors.DEFAULT_GREEN,
-      alignSelf: "center",
-      paddingHorizontal: 20,
-      justifyContent: "space-between",
-      alignItems: "center",
-      borderRadius: 10,
-      height: Display.setHeight(7),
-      marginTop: 10,
-    },
-    checkoutText: {
-      fontSize: 16,
-      fontWeight: "600",
-      lineHeight: 16 * 1.4,
-      color: Colors.DEFAULT_WHITE,
-      marginLeft: 8,
-    },
+  
+    
+ 
+
     emptyCartContainer: {
       flex: 1,
       justifyContent: "center",
